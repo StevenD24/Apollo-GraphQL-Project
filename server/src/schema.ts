12d849a2974be969e472a2bc99f74021d2f4275c
Steven Duong <stevenduongx@gmail.com>
2024-05-03 16:55:55 -0600

    feat: create schema.ts

---
+++ server/src/schema.ts
@@ -0,0 +1,32 @@
+import gql from "graphql-tag"
+
+export const typeDefs = gql`
+type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
+}
+
+"A track is a group of Modules that teaches about a specific topic"
+type Track {
+    id: ID!
+    "the track's title"
+    title: String!
+    "the track's main author"
+    author: Author!
+    "the track's main illustration to display on a card"
+    thumbnail: String
+    "the track's approximate length to complete, in minutes"
+    length: Int
+    "the number of modules this track contains"
+    modulesCount: Int
+}
+
+"Author of a complete Track"
+type Author {
+    id: ID!
+    "author's first and last name"
+    name: String!
+    "author's profile picture url"
+    photo: String
+}
+`;
