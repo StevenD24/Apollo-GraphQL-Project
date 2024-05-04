0af8a3b4d90de19cade0f17ea55fd0543e48c523
Steven Duong <stevenduongx@gmail.com>
2024-05-03 23:17:00 -0600

    connect schema, resolvers, and datasources in server config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,39 +1,28 @@
 // TODO: Build our server
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { addMocksToSchema } from "@graphql-tools/mock";
-import { makeExecutableSchema } from "@graphql-tools/schema";
 import { typeDefs } from "./schema";
-
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(6)],
-  }),
-  Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
-    author: () => {
-      return {
-        name: "Grumpy Cat",
-        photo:
-          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
-      };
-    },
-    thumbnail: () =>
-      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+import { resolvers } from "./resolvers/resolvers";
+import { TrackAPI } from "./datasources/track-api";
 
 async function startApolloServer() {
   const server = new ApolloServer({
-    schema: addMocksToSchema({
-      schema: makeExecutableSchema({ typeDefs }),
-      mocks,
-    }),
+    typeDefs,
+    resolvers,
   });
-  const { url } = await startStandaloneServer(server);
+  const { url } = await startStandaloneServer(server, {
+    // context function returns an object that all the resolvers will share
+    context: async () => {
+      // use the RESTDataSource cache and pass it to TrackAPI
+      const { cache } = server;
+      return {
+        // access data sources
+        dataSources: {
+          trackAPI: new TrackAPI({ cache }),
+        },
+      };
+    },
+  });
   console.log(`
     🚀  Server is running!
     📭  Query at ${url}
@@ -41,3 +30,29 @@ async function startApolloServer() {
 }
 
 startApolloServer();
+
+// const mocks = {
+//   Query: () => ({
+//     tracksForHome: () => [...new Array(6)],
+//   }),
+//   Track: () => ({
+//     id: () => "track_01",
+//     title: () => "Astro Kitty, Space Explorer",
+//     author: () => {
+//       return {
+//         name: "Grumpy Cat",
+//         photo:
+//           "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+//       };
+//     },
+//     thumbnail: () =>
+//       "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+//     length: () => 1210,
+//     modulesCount: () => 6,
+//   }),
+// };
+
+// schema: addMocksToSchema({
+//     schema: makeExecutableSchema({ typeDefs }),
+//     mocks,
+// }),
