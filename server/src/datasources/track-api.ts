4d07d9de5eadf80769942fd71dc225b3d84f9325
Steven Duong <stevenduongx@gmail.com>
2024-05-03 22:09:04 -0600

    add HTTP methods to our RESTDataSource

--- server/src/datasources/track-api.ts
+++ server/src/datasources/track-api.ts
@@ -2,4 +2,13 @@ import { RESTDataSource } from "@apollo/datasource-rest";
 
 export class TrackAPI extends RESTDataSource {
     baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+
+    getTracksForHome() {
+        // make a get req. to the tracks endpoint
+        return this.get('tracks');
+    }
+
+    getAuthor(authorId: string) {
+        return this.get(`author/${authorId}`);
+    }
 }
